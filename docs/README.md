a8dd311dd5f8f81e336400df4c659fd1f9103c29
rsiowkf <e1156608@u.nus.edu>
2025-08-24 14:36:27 +0800

    Added Ryan.java and changed Duke's name to Siao

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Siao User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Siao.java
@@ -0,0 +1,7 @@
+public class Siao {
+    public static void main(String[] args) {
+      System.out.println("Hello! I'm Siao!");
+      System.out.println("What can I do for you?");
+      System.out.println("Bye. Hope to see you again soon!");
+    }
+}
