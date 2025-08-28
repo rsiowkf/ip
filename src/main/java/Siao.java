78154d4478999f820746900b57ea7797d000c513
ryan <e1156608@u.nus.edu>
2025-08-28 15:38:53 +0800

    Removed an exclamation mark

--- src/main/java/Siao.java
+++ src/main/java/Siao.java
@@ -2,6 +2,6 @@ public class Siao {
     public static void main(String[] args) {
       System.out.println("Hello! I'm Siao!");
       System.out.println("What can I do for you?");
-      System.out.println("Bye. Hope to see you again soon!");
+      System.out.println("Bye. Hope to see you again soon");
     }
 }
