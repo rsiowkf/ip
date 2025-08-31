26e433f9fe25dd75490c40da02a0562c15e6f4ec
rsiowkf <e1156608@u.nus.edu>
2025-08-31 15:57:21 +0800

    level-1: Echo edits

--- src/main/java/Siao.java
+++ src/main/java/Siao.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Siao {
     public static void main(String[] args) {
       System.out.println("Hello! I'm Siao!");
+
+      Scanner input = new Scanner(System.in);
       System.out.println("What can I do for you?");
+      String line = input.nextLine();
+
+      while(!line.equalsIgnoreCase("bye")){
+          System.out.printf("---------------------------------\n" + line + "\n---------------------------------\n");
+          line = input.nextLine();
+      }
+      System.out.println("---------------------------------");
       System.out.println("Bye. Hope to see you again soon!");
     }
 }
