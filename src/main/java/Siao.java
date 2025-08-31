7ccc17f2f83420defd8c2bbad649eaad768090ac
rsiowkf <e1156608@u.nus.edu>
2025-08-31 16:29:51 +0800

    level 2: Add, List

--- src/main/java/Siao.java
+++ src/main/java/Siao.java
@@ -1,6 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Siao {
+
+    public static void  printDividerLine(){
+        System.out.println("---------------------------------");
+    }
+
+    public static void printList(String[] list) {
+        printDividerLine();
+        int j = 1;
+        for (int i = 0; i < list.length; i++) {
+            System.out.printf(j + "." + list[i] + "\n");
+            j++;
+        }
+        printDividerLine();
+    }
+
+    public static void printAction(String action){
+        printDividerLine();
+        System.out.printf("added: " + action + "\n");
+        printDividerLine();
+    }
+
     public static void main(String[] args) {
       System.out.println("Hello! I'm Siao!");
 
@@ -8,10 +30,22 @@ public class Siao {
       System.out.println("What can I do for you?");
       String line = input.nextLine();
 
+      String[] list = new String[100];
+      int matchCount = 0;
+
       while(!line.equalsIgnoreCase("bye")){
-          System.out.printf("---------------------------------\n" + line + "\n---------------------------------\n");
+          printAction(line);
+          list[matchCount] = line;
+          matchCount++;
           line = input.nextLine();
+
+          if (line.equalsIgnoreCase("list")){
+              printList(Arrays.copyOf(list, matchCount));
+              line = input.nextLine();
+          }
       }
+
+
       System.out.println("---------------------------------");
       System.out.println("Bye. Hope to see you again soon!");
     }
