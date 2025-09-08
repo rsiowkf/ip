b493861d12facfe7d9260dcc3fc73b9236e51581
rsiowkf <e1156608@u.nus.edu>
2025-09-08 13:28:39 +0800

    Added a Constants class to store all the magic numbers and constants

--- src/main/java/Siao.java
+++ src/main/java/Siao.java
@@ -3,22 +3,12 @@ import java.util.ArrayList;
 
 public class Siao {
 
-
-    private static final String DIVIDER = "---------------------------------";
-    private static final String WELCOME_MESSAGE = "Hello I'm Siao!\nWhat can I do for you today?\n";
-    private static final String GOODBYE_MESSAGE = "Bye bye! Do remember to complete your tasks!";
-    private static final int DEADLINE_INDEX = 9;
-    private static final int EVENT_INDEX = 6;
-    private static final int TODO_INDEX = 5;
-
-
-
     public static void  printDividerLine(){
-        System.out.println(DIVIDER);
+        System.out.println(Constants.DIVIDER);
     }
 
     public static void main(String[] args) {
-        System.out.print(WELCOME_MESSAGE);
+        System.out.print(Constants.WELCOME_MESSAGE);
 
         Scanner input = new Scanner(System.in);
         String line = input.nextLine();
@@ -50,7 +40,7 @@ public class Siao {
                     break;
 
                 case "deadline":
-                    String deadlineDescription = line.substring(DEADLINE_INDEX);
+                    String deadlineDescription = line.substring(Constants.DEADLINE_INDEX);
                     Deadline newDeadline = new Deadline(deadlineDescription);
                     list.add(newDeadline);
                     newDeadline.printAddedTask();
@@ -58,7 +48,7 @@ public class Siao {
                     break;
 
                 case "event":
-                    String  eventDescription = line.substring(EVENT_INDEX);
+                    String  eventDescription = line.substring(Constants.EVENT_INDEX);
                     Event newEvent = new Event(eventDescription);
                     list.add(newEvent);
                     newEvent.printAddedTask();
@@ -66,7 +56,7 @@ public class Siao {
                     break;
 
                 case "todo":
-                    String todoDescription = line.substring(TODO_INDEX);
+                    String todoDescription = line.substring(Constants.TODO_INDEX);
                     Todo newTodo = new Todo(todoDescription);
                     list.add(newTodo);
                     newTodo.printAddedTask();
@@ -82,7 +72,7 @@ public class Siao {
             }
         }
 
-        System.out.println(DIVIDER);
-        System.out.println(GOODBYE_MESSAGE);
+        System.out.println(Constants.DIVIDER);
+        System.out.println(Constants.GOODBYE_MESSAGE);
     }
 }
