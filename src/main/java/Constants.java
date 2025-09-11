56b12f227871721259b16db5017368a8487f8f2b
ryan <e1156608@u.nus.edu>
2025-09-11 10:53:53 +0800

    added Constants and changed welcome message

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,13 +1,29 @@
 public class Constants {
     public static final String DIVIDER = "---------------------------------";
     public static final String END_OF_LIST = "-----------End Of List-----------";
-    public static final String WELCOME_MESSAGE = "Hello I'm Siao!\nWhat can I do for you today?\n";
     public static final String GOODBYE_MESSAGE = "Bye bye! Do remember to complete your tasks!";
     public static final String EMPTY_ERROR_MESSAGE = "YOUR TASK IS NOT SUPPOSED TO BE EMPTY!\nAdd another task please!";
-    public static final String EMPTY_DESC_MESSAGE = "Add another task please!";
+    public static final String EMPTY_DESC_MESSAGE = "☹ OOPS!! Add a task please!";
     public static final String ILLEGAL_ARGUMENT_MESSAGE = "☹ OOPS!!! The description cannot be empty.";
     public static final String ILLEGAL_DEADLINE_MESSAGE = "☹ OOPS!!! It's not a deadline without a deadline!";
     public static final String ILLEGAL_START_MESSAGE = "☹ OOPS!!! So when's your event starting?";
     public static final String ILLEGAL_END_MESSAGE = "☹ OOPS!!! So when's your event ending?";
-    public static final int TODO_INDEX = 5;
+    public static final String ILLEGAL_MESSAGE = "☹ OOPS!! What are you trying to tell me?";
+    public static final String EMPTY_LIST = "Erm your list is empty, what are you tryna do?";
+
+
+    public static final String WELCOME_MESSAGE =
+            "____________________________________________________________\n" +
+                    "   _____ _                   \n" +
+                    "  / ____| |                  \n" +
+                    " | (___ | |__   __ _ _   _   \n" +
+                    "  \\___ \\| '_ \\ / _` | | | |  \n" +
+                    "  ____) | | | | (_| | |_| |  \n" +
+                    " |_____/|_| |_|\\__,_|\\__, |  \n" +
+                    "                       __/ |  \n" +
+                    "                      |___/   \n" +
+                    "____________________________________________________________\n" +
+                    " Hello, I'm Siao!  (｡◕‿◕｡) \n" +
+                    " What can I do for you today?\n" +
+                    "____________________________________________________________\n";
 }
