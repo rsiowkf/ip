9dfda8889094c02e20486993de65688ce6d4444a
ryan <e1156608@u.nus.edu>
2025-09-30 17:51:13 +0800

    created parser class for storage items

--- src/main/java/Siao/Parser/Parser.java
+++ src/main/java/Siao/Parser/Parser.java
@@ -1,4 +1,46 @@
 package Siao.Parser;
 
+import Siao.task.Deadline;
+import Siao.task.Event;
+import Siao.task.Task;
+import Siao.task.Todo;
+
 public class Parser {
+    public Task parseTask(String line) {
+        String[] parts =  line.split("\\|");
+        String type = parts[0];
+        int isDone =  Integer.parseInt(parts[1]);
+        String desc = parts[2];
+
+        Task task;
+
+        switch (type) {
+        case "T":
+            task = new Todo(desc);
+            if (isDone == 1) {
+                task.markDone();
+            }
+            break;
+        case "D":
+            String deadlineDesc = parts[2] + "/by" + parts[3];
+            task = new Deadline(deadlineDesc);
+            if (isDone == 1) {
+                task.markDone();
+            }
+            break;
+        case "E":
+            String eventDesc = parts[2] + "/from" + parts[3] + "/to" + parts[4];
+            task = new Event(eventDesc);
+            if (isDone == 1) {
+                task.markDone();
+            }
+            break;
+        default:
+            return null;
+        }
+        if (isDone == 1) {
+            task.markDone();
+        }
+        return task;
+    }
 }
