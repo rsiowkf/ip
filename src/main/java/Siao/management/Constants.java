6b3df9222ae875f35749b6d762ccf5db8bc826b7
ryan <e1156608@u.nus.edu>
2025-10-03 15:14:38 +0800

    Constants class: removed unused constant

--- src/main/java/Siao/management/Constants.java
+++ src/main/java/Siao/management/Constants.java
@@ -4,7 +4,6 @@ public class Constants {
     public static final String DIVIDER = "---------------------------------";
     public static final String END_OF_LIST = "-----------End Of List-----------";
     public static final String GOODBYE_MESSAGE = "Bye bye! Do remember to complete your tasks!";
-    public static final String EMPTY_ERROR_MESSAGE = "YOUR TASK IS NOT SUPPOSED TO BE EMPTY!\nAdd another task please!";
     public static final String EMPTY_DESC_MESSAGE = "☹ OOPS!! Add a task please!";
     public static final String ILLEGAL_ARGUMENT_MESSAGE = "☹ OOPS!!! The description cannot be empty.";
     public static final String ILLEGAL_DEADLINE_MESSAGE = "☹ OOPS!!! It's not a deadline without a deadline!";
